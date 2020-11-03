48b1267cf92ebfcc953698fc02a417362c8b7035
Tyler Hennig <tyhennig@gmail.com>
2020-11-03 10:47:54 -0600

    Created .cs file

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+public class Stock
+{
+    private string ticker;
+    private string name;
+    private decimal latestPrice;
+    private decimal dailyHigh;
+    private decimal dailyLow;
+
+    Stock(string ticker, decimal latestPrice)
+    {
+        this.ticker = ticker;
+        this.latestPrice = latestPrice;
+    }
+
+    private void updatePrice()
+    {
+
+    }
+
+
+}
+
+public class User
+{
+    private string username;
+    private string password;
+
+}
+
+public class TradingBot
+{
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
