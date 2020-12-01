cee916388ffc02197ff56400e24421412b9ed88c
tyler hennig <tyhennig@gmail.com>
2020-12-01 16:52:37 -0600

    removed unneeded comments

--- Pages/Movers.cs
+++ Pages/Movers.cs
@@ -56,42 +56,21 @@ namespace StockBot
 
             lastUser = Display.currentUser;
         }
-        //public override void display()
-        //{
-        //    Console.WriteLine("Top 25 movers from Yahoo Finance are: ");
-            
-        //    for (int i = 0; i < TradingBot.movers.Count; i++)
-        //    {
-        //        Console.WriteLine(string.Format("{0} - \t{1} \t{2} \t{3} \t{4}% \t{5} \t{6} \t{7} \t{8} \t{9} \t{10}",
-        //            i + 1, TradingBot.movers[i].symbol, TradingBot.movers[i].shortName, TradingBot.movers[i].regularMarketPrice.raw, TradingBot.movers[i].regularMarketChange.raw,
-        //            TradingBot.movers[i].regularMarketChangePercent.raw, TradingBot.movers[i].regularMarketVolume.raw, TradingBot.movers[i].averageDailyVolume3Month.raw, TradingBot.movers[i].marketCap.raw, TradingBot.movers[i].fiftyTwoWeekLow.raw,
-        //            TradingBot.movers[i].fiftyTwoWeekHigh.raw, TradingBot.movers[i].regularMarketOpen.raw));
-        //    }
-        //}
+        
 
         public override void run()
         {
-            
-            //running = true;
-            //var worker = new Thread(() =>
-            //{
-            //    while (running)
-            //    {
-                    
-                    
-                    //if (running)
-                        if (Display.currentUser != lastUser || Display.currentUser == null)
-                        {
-                            bot.FetchMovers();
-                            Console.Clear();
-                            updateElements();
+           
+            if (Display.currentUser != lastUser || Display.currentUser == null)
+            {
+                bot.FetchMovers();
+                Console.Clear();
+                updateElements();
                 selectedElement = elements[0];
+                lastUser = Display.currentUser;
             }
 
-                    
-                    //Thread.Sleep(1000);
-            //    }
-            //});
+       
 
             //worker.Start();
             display();
